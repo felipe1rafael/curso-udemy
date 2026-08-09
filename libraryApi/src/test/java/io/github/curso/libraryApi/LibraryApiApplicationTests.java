00077d138ff4420afb80b165219413bcc8b6a7fa
felipe rafael <fr2794763@gmail.com>
2026-08-08 22:26:09 -0300

    teste commit

---
+++ libraryApi/src/test/java/io/github/curso/libraryApi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.curso.libraryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
